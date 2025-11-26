084e487bc2b761165c801b19aed0b7ee2637fa20
Maxime Le Mouel <maxime.lemouel1@gmail.com>
2025-11-26 19:00:13 +0100

    3eme commit

--- wp-content/themes/Site/front-page.php
+++ wp-content/themes/Site/front-page.php
@@ -9,20 +9,58 @@
     <h3>Nos Produits Phare</h3>
 
     <div class="product-grid">
-        <div class="product-card">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/pommes.jpg" alt="Pommes">
-            <h4>Pommes</h4>
-        </div>
+        <?php
+        $args = array(
+            'post_type'      => 'product',
+            'posts_per_page' => 6,
+            'orderby'        => 'date',
+            'order'          => 'DESC',
+        );
 
-        <div class="product-card">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/carottes.jpg" alt="Carottes">
-            <h4>Carottes</h4>
-        </div>
+        $loop = new WP_Query($args);
 
-        <div class="product-card">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/tomates.jpg" alt="Tomates">
-            <h4>Tomates</h4>
-        </div>
+        if ($loop->have_posts()) :
+            while ($loop->have_posts()) : $loop->the_post();
+                global $product;
+        ?>
+                <div class="product-card">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php 
+                        if (has_post_thumbnail()) {
+                            the_post_thumbnail('medium');
+                        } else {
+                            echo '<img src="' . wc_placeholder_img_src() . '" alt="Produit">';
+                        }
+                        ?>
+                    </a>
+
+                    <h4>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+                    </h4>
+
+                    <div class="product-price">
+                        <?php echo $product->get_price_html(); ?>
+                    </div>
+
+                    <div class="product-button">
+                        <?php woocommerce_template_loop_add_to_cart(); ?>
+                    </div>
+                </div>
+        <?php
+            endwhile;
+            wp_reset_postdata();
+        else :
+            echo '<p>Aucun produit disponible pour le moment.</p>';
+        endif;
+        ?>
+    </div>
+
+    <div class="shop-link" style="text-align: center; margin-top: 30px;">
+        <a href="<?php echo get_permalink(wc_get_page_id('shop')); ?>" class="button">
+            Voir tous les produits →
+        </a>
     </div>
 </section>
 
